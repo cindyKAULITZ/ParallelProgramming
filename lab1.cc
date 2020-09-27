ef7642604dd5cae01985ae31b97d56f176aba38e
cindyKAULITZ <tony320cindy@gmail.com>
2020-09-28 00:03:45 +0800

    v1.1

--- lab1.cc
+++ lab1.cc
@@ -10,6 +10,9 @@ unsigned long long compute_single(unsigned long long x, unsigned long long r_squ
 	return y; 
 }
 
+
+unsigned long long  Reduce_sum;
+
   
 int main(int argc, char** argv) {
 	if (argc != 3) {
@@ -21,11 +24,9 @@ int main(int argc, char** argv) {
 	unsigned long long k = atoll(argv[2]);
 
 	int pid, np;
-	unsigned long long elements_per_process, sub_start, r_squre; 
+	unsigned long long elements_per_process, sub_start, r_squre;
+    unsigned long long i; 
     
-    // pre-calculation
-    r_squre = r*r;
-    // elements_per_process =  floorl(r + np - 1) / np; 
 
     // np -> number of processes 
     // pid -> process id 
@@ -38,31 +39,25 @@ int main(int argc, char** argv) {
     MPI_Comm_rank(MPI_COMM_WORLD, &pid); 
     MPI_Comm_size(MPI_COMM_WORLD, &np); 
 
+    // pre-calculation
+    r_squre = r*r;
+    elements_per_process =  floorl(r + np - 1) / np; 
+    sub_start = pid * elements_per_process;
+    unsigned long long sub_end = sub_start + elements_per_process;
+
 	// master process 
-    if (pid == 0) { 
-        int i; 
-        
-        // r_squre = r*r;
-        elements_per_process =  (r + np - 1) / np; 
-
-
+    if (pid == 0) {         
         // check if more than 1 processes are run 
-        if (np > 1) { 
-            // distributes the necessary information for each processor
-            for (i = 1; i < np ; ++i) { 
-                sub_start = i * elements_per_process; 
-  
-                MPI_Send(&elements_per_process, 
-                         1, 
-						 MPI_UNSIGNED_LONG_LONG, i, 0, 
-                         MPI_COMM_WORLD); 
-
-                MPI_Send(&sub_start, 
-                         1, 
-                         MPI_UNSIGNED_LONG_LONG, i, 1, 
-                         MPI_COMM_WORLD); 
-            } 
-        } 
+        // if (np > 1) { 
+        //     // distributes the necessary information for each processor
+        //     // for (i = 1; i < np ; ++i) { 
+        //     //     sub_start += elements_per_process; 
+        //     //     MPI_Send(&sub_start, 
+        //     //              1, 
+        //     //              MPI_UNSIGNED_LONG_LONG, i, 1, 
+        //     //              MPI_COMM_WORLD); 
+        //     // } 
+        // } 
   
         // own calculation of master process 
         unsigned long long sum = 0; 
@@ -78,52 +73,43 @@ int main(int argc, char** argv) {
                      MPI_ANY_SOURCE, 0, 
                      MPI_COMM_WORLD, 
                      &status); 
-            int sender = status.MPI_SOURCE; 
+            // int sender = status.MPI_SOURCE; 
             sum += tmp; 
-            sum %= k;
         } 
 
+        // sum += Reduce_sum;
         // prints the final sum of pixels 
 		printf("%llu\n", (sum << 2) % k);
     } 
     // slave processes 
     else { 
-        MPI_Recv(&elements_per_process, 
-                 1, 
-				 MPI_UNSIGNED_LONG_LONG, 0, 0, 
-                 MPI_COMM_WORLD, 
-                 &status); 
-
-		MPI_Recv(&sub_start, 
-                 1, 
-				 MPI_UNSIGNED_LONG_LONG, 0, 1, 
-                 MPI_COMM_WORLD, 
-                 &status); 
+		// MPI_Recv(&sub_start, 
+        //          1, 
+		// 		 MPI_UNSIGNED_LONG_LONG, 0, 1, 
+        //          MPI_COMM_WORLD, 
+        //          &status); 
 
         // calculates its partial sum of pixel
         unsigned long long partial_sum = 0; 
-		unsigned long long sub_end = sub_start + elements_per_process;
 		if (pid == (np-1)) { sub_end = r; }
         
-		// subtask
-		for (unsigned long long i = sub_start; i < sub_end; ++i) {
+		// ubtask
+		for (i = sub_start; i < sub_end; ++i) {
             partial_sum += compute_single(i, r_squre); 
         }
+        partial_sum %= k;
   
         // sends the partial sum to the root process 
-        MPI_Send(&partial_sum, 1, MPI_UNSIGNED_LONG_LONG, 
-                 0, 0, MPI_COMM_WORLD); 
+        MPI_Send(&partial_sum, 
+                 1, 
+                 MPI_UNSIGNED_LONG_LONG, 0, 0, 
+                 MPI_COMM_WORLD); 
+        // MPI_Reduce(&partial_sum, &Reduce_sum, 1, MPI_UNSIGNED_LONG_LONG, MPI_SUM, 0,
+        //            MPI_COMM_WORLD);
     } 
   
     // cleans up all MPI state before exit of process 
-    MPI_Finalize(); 
-  
+    MPI_Finalize();   
     return 0; 
 }
 
-
-// nproc=12
-// r=4294967295
-// k=1099511627775
-// answer=576603832986
-// timelimit=20
