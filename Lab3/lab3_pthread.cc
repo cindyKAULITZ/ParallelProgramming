562e05e56f285dcfa2a33f73da9b669fc0f6ffca
cindyKAULITZ <tony320cindy@gmail.com>
2020-10-27 18:07:56 +0800

    lab3_pthread update

--- Lab3/lab3_pthread.cc
+++ Lab3/lab3_pthread.cc
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <pthread.h>
 
+<<<<<<< Updated upstream
 unsigned long long pixels = 0;
 unsigned long long ncpus = 0;
 unsigned long long r = 0;
@@ -31,6 +32,51 @@ void* Compute(void* sub){
 	}
 	// printf()
 	// pixels += y;
+=======
+
+#define METHOD_1
+// #define METHOD_2
+
+unsigned long long r_square;
+unsigned long long r;
+unsigned long long k;
+unsigned long long batch;
+unsigned long long pixels = 0;
+
+#ifdef METHOD_1
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+#endif
+
+
+typedef struct data {
+    unsigned long long thread_id;
+    unsigned long long pixel = 0;
+} data;
+
+
+void* sub_task(void *arg) {
+	data *pdata = (data*)arg;
+	unsigned long long sub_start = pdata->thread_id * batch;
+	unsigned long long sub_end = sub_start + batch;
+	if (sub_end > r) { sub_end = r;}
+
+	unsigned long long base = r_square - (sub_start*sub_start);
+	unsigned long long xx = 0;
+	unsigned long long sub_pixel = 0;
+	for(unsigned long long ele_count = sub_start; ele_count < sub_end; ele_count++){
+		base = base - xx;
+		sub_pixel += ceil(sqrtl(base));
+		xx = 2*ele_count + 1;		
+	}
+	sub_pixel %= k;
+	pdata->pixel = sub_pixel;
+
+#ifdef METHOD_1
+	pthread_mutex_lock(&mutex);
+	pixels += pdata->pixel;
+	pthread_mutex_unlock(&mutex);
+#endif
+>>>>>>> Stashed changes
 	pthread_exit(NULL);
 }
 
@@ -41,6 +87,7 @@ int main(int argc, char** argv) {
 		return 1;
 	}
 	r = atoll(argv[1]);
+<<<<<<< Updated upstream
 	unsigned long long k = atoll(argv[2]);
 	// unsigned long long pixels = 0;
 	cpu_set_t cpuset;
@@ -66,8 +113,45 @@ int main(int argc, char** argv) {
 
 	
 	pixels %= k;
-	// printf("pixels = %llu\n", pixels);
+=======
+	k = atoll(argv[2]);
 
-	printf("%llu\n", (4 * pixels) % k);
+    // acquire cpu number
+	cpu_set_t cpuset;
+	sched_getaffinity(0, sizeof(cpuset), &cpuset);
+	unsigned long long ncpus = CPU_COUNT(&cpuset);
+    
+	// compute batch
+	batch = ((r-1)/(ncpus))+1;
+	r_square = r*r;
+
+    pthread_t threads[ncpus];
+	data datas[ncpus];
+
+	for (int i = 0 ; i <ncpus; i++){
+		// part 0 is done by main thread
+		datas[i].thread_id = i;
+  		pthread_create(&threads[i], NULL, sub_task, (void*) &datas[i]);
+	}
+
+	for (int i = 0 ; i <ncpus; i++){
+		pthread_join(threads[i], NULL);
+	}
+
+#ifdef METHOD_2
+	for (int i = 0 ; i <ncpus; i++){
+		pixels += datas[i].pixel;
+	}
+#endif
+
+>>>>>>> Stashed changes
+	// printf("pixels = %llu\n", pixels);
+	printf("%llu\n", (4 * (pixels)) % k);
+
+#ifdef METHOD_1
 	pthread_mutex_destroy(&mutex);
+#endif
+
 }
+
+
