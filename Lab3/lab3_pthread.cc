639ed9c0b017636aa452de7dc05b117881fd4389
cindyKAULITZ <tony320cindy@gmail.com>
2020-10-27 16:09:47 +0800

    create threads

--- Lab3/lab3_pthread.cc
+++ Lab3/lab3_pthread.cc
@@ -3,33 +3,54 @@
 #include <math.h>
 #include <pthread.h>
 
+unsigned long long pixels = 0;
+unsigned long long ncpus = 0;
+unsigned long long r = 0;
+unsigned long long x = 0;
+unsigned long long xx = -1;
+unsigned long long base = 0;
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+void* Compute(void* arg){
+	for (; x < r; x++) {
+		// y = ceil(sqrtl(base));
+		// pthread_mutex_lock(&mutex);
+		xx += 2;
+		// pthread_mutex_unlock(&mutex);
+		pixels = pixels + ceil(sqrtl(base));
+		base = base - xx;
+		
+		// pthread_mutex_lock(&mutex);
+		// base -= 2*x -1;
+		// xx = (x*2) + 1;
+		// pthread_mutex_unlock(&mutex);
+	}
+	pthread_exit(NULL);
+}
+
+
 int main(int argc, char** argv) {
 	if (argc != 3) {
 		fprintf(stderr, "must provide exactly 2 arguments!\n");
 		return 1;
 	}
-	unsigned long long r = atoll(argv[1]);
+	r = atoll(argv[1]);
 	unsigned long long k = atoll(argv[2]);
-	unsigned long long pixels = 0;
-	// cpu_set_t cpuset;
-	// sched_getaffinity(0, sizeof(cpuset), &cpuset);
-	// unsigned long long ncpus = CPU_COUNT(&cpuset);
-
-	unsigned long long y, xx=0;
-	unsigned long long base = r*r ;
-	pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
-
-	for (unsigned long long x = 0; x < r; x++) {
-		// y = ceil(sqrtl(base));
-		base = base - xx;
-		pixels = pixels + ceil(sqrtl(base));
-		// pthread_mutex_unlock(&mutex);
-		// pthread_mutex_lock(&mutex);
-		pthread_mutex_lock(&mutex);
-		// base -= 2*x -1;
-		xx = 2*x + 1;
-		pthread_mutex_unlock(&mutex);
+	// unsigned long long pixels = 0;
+	cpu_set_t cpuset;
+	sched_getaffinity(0, sizeof(cpuset), &cpuset);
+	ncpus = CPU_COUNT(&cpuset);
+	pthread_t t[ncpus]; 
+	// unsigned long long y, xx=0;
+	base = r*r ;
+	
+	for (int i = 0 ; i <ncpus; i++){
+		// write Compute function to calculate pixels
+  		pthread_create(&t[i], NULL, Compute, NULL);
+		pthread_join(t[i], NULL);
 	}
+
+	
 	pixels %= k;
 	// printf("pixels = %llu\n", pixels);
 
