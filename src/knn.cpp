b154dc9f60a4040cef2b4dae71e77ed6dd368fff
Yuan-Chuan-YUE <qyyuan@pllab.cs.nthu.edu.tw>
2021-01-08 14:08:18 +0800

    add intrinsic but a little effect only

--- src/knn.cpp
+++ src/knn.cpp
@@ -4,16 +4,34 @@
 #include "dataset.h"
 #include <iostream>
 #include "debug.h"
+#include <emmintrin.h>
+#include <smmintrin.h>
+#include <array>
 #include <omp.h>
+
+double intrinSub(std::array<double, 2> a, std::array<double, 2> b){
+    __m128d minute = _mm_load_pd(a.data());
+    __m128d sub = _mm_load_pd(b.data());
+    minute = _mm_sub_pd(minute, sub);
+    minute = _mm_mul_pd(minute, minute);
+    double ret[2];
+    _mm_store_pd(ret, minute);
+    return ret[0] + ret[1];
+}
 
 double GetSquaredDistance(DatasetPointer train, size_t trainExample, DatasetPointer target, size_t targetExample) {
 	assert(train->cols == target->cols);
 	double sum = 0;
 	double difference;
-	for(size_t col = 0; col < train->cols; col++) {
+    size_t col = 0;
+	for(; col < train->cols; col+= 2) {
+		sum += intrinSub({train->pos(trainExample, col), train->pos(trainExample, col + 1)}, 
+                { target->pos(targetExample, col), target->pos(targetExample, col + 1) });
+	}
+    if(train->cols % 2 == 1){
 		difference = train->pos(trainExample, col) - target->pos(targetExample, col);
 		sum += difference * difference;
-	}
+    }
 	return sum;
 }
 
