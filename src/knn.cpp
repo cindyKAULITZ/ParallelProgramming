f20a67e027ba24102a20608dfcf7e0d148c2ca68
Yuan-Chuan-YUE <qyyuan@pllab.cs.nthu.edu.tw>
2021-01-09 17:46:05 +0800

    imitate opencv + parcial_sort, very fast

--- src/knn.cpp
+++ src/knn.cpp
@@ -55,7 +55,7 @@ KNNResults KNN::run(int k, DatasetPointer target) {
     unsigned long long tRows = target->rows;
     unsigned long long dRows = data->rows;
     double * dist = (double *)malloc(sizeof(double) * tRows * dRows);
-    double * ddist = (double *)malloc(sizeof(double) * tRows * dRows);
+    double * ddist = (double *)malloc(sizeof(double) * tRows * k);
     int * idx = (int *)malloc(sizeof(int) * tRows * dRows);
     for(unsigned long long t = 0;t < tRows;++t){
         std::iota(idx + t * dRows, idx + (t + 1) * dRows, 0);
@@ -63,41 +63,59 @@ KNNResults KNN::run(int k, DatasetPointer target) {
 	//std::pair<double, int> * squaredDistances = new std::pair<double, int>[tRows * dRows];
     std::chrono::steady_clock::time_point b = std::chrono::steady_clock::now();
     const unsigned int trainTileSize = 64;
-    const unsigned int testTileSize = 128;
-    for(unsigned long long testTileBegin = 0; testTileBegin < dRows; testTileBegin += testTileSize){
+    const unsigned int testTileSize = 256;
+    //for(unsigned long long testTileBegin = 0; testTileBegin < dRows; testTileBegin += testTileSize){
 //#pragma omp parallel for schedule(static) num_threads(8)
-        for(unsigned long long trainTileBegin = 0; trainTileBegin < dRows; trainTileBegin += trainTileSize){
-            for(size_t targetExample = testTileBegin; targetExample < tRows && targetExample < testTileBegin + testTileSize; targetExample++) {
-                /*
+        //for(unsigned long long trainTileBegin = 0; trainTileBegin < dRows; trainTileBegin += trainTileSize){
+    for (size_t trainExample = 0; trainExample < dRows; trainExample++) {
+        #pragma omp parallel for schedule(static, 256) num_threads(8)
+        for(size_t targetExample = 0; targetExample < tRows; targetExample++) {
+            /*
 #ifdef DEBUG_KNN
 if (targetExample % 100 == 0)
 DEBUGKNN("Target %lu of %lu\n", targetExample, tRows);
 #endif
 */
-                //Find distance to all examples in the training set
-                for (size_t trainExample = trainTileBegin; trainExample < trainTileBegin + trainTileSize && trainExample < dRows; trainExample++) {
-                    dist[targetExample * dRows + trainExample] = GetSquaredDistance(data, trainExample, target, targetExample);
-                    //squaredDistances[targetExample * dRows + trainExample].second = trainExample;
-                }
+            //Find distance to all examples in the training set
+            //dist[targetExample * dRows + trainExample] = GetSquaredDistance(data, trainExample, target, targetExample);
+            //squaredDistances[targetExample * dRows + trainExample].second = trainExample;
+            int d = 0;
+            double sum = 0;
+            for(; d < data->cols - 4; d += 4){
+                double t0 = data->pos(trainExample, d) - target->pos(targetExample, d);
+                double t1 = data->pos(trainExample, d + 1) - target->pos(targetExample, d + 1);
+                double t2 = data->pos(trainExample, d + 2) - target->pos(targetExample, d + 2);
+                double t3 = data->pos(trainExample, d + 3) - target->pos(targetExample, d + 3);
+                sum += t0 * t0 + t1 * t1 + t2 * t2 + t3 * t3;
             }
+            for(; d < data->cols; ++d){
+                double t0 = data->pos(trainExample, d) - target->pos(targetExample, d);
+                sum += t0 * t0;
+            }
+            dist[targetExample * dRows + trainExample] = sum;
         }
-
     }
+    //}
+
+    //}
     std::chrono::steady_clock::time_point e = std::chrono::steady_clock::now();
     std::cout << "Compute Distance time: " << static_cast<double>(std::chrono::duration_cast<std::chrono::milliseconds>(e - b).count()) / 1000 << "s.\n";
 
     std::chrono::steady_clock::time_point b1 = std::chrono::steady_clock::now();
-#pragma omp parallel for schedule(static) num_threads(8)
+#pragma omp parallel for schedule(static, 256) num_threads(8)
     for(size_t targetExample = 0; targetExample < tRows; targetExample++) {
-        std::sort(idx + targetExample * dRows, idx + targetExample * dRows + dRows,
+        std::partial_sort(idx + targetExample * dRows, 
+                idx + targetExample * dRows + k,
+                idx + (targetExample + 1) * dRows,
                 [&dist, &targetExample, &dRows](const int & a,const int & b){
-                    return dist[targetExample * dRows + a] < dist[targetExample * dRows + b];
+                return dist[targetExample * dRows + a] < dist[targetExample * dRows + b];
                 });
     }
-    for(size_t targetExample = 0; targetExample < tRows; targetExample++) {
-        for(int i = 0;i < dRows;++i){
-            ddist[targetExample * dRows + i] = dist[idx[i]];
-        }
+#pragma omp parallel for num_threads(8)
+    for(unsigned long long t = 0; t < tRows * k; t++) {
+        int targetExample = t / tRows;
+        int i = t % tRows;
+        ddist[targetExample * k + i] = dist[idx[i]];
     }
     free(dist);
     dist = ddist;
@@ -107,35 +125,35 @@ DEBUGKNN("Target %lu of %lu\n", targetExample, tRows);
 
 
 #pragma omp parallel for schedule(static) num_threads(8)
-	for(size_t targetExample = 0; targetExample < tRows; targetExample++) {
+    for(size_t targetExample = 0; targetExample < tRows; targetExample++) {
 
 
-		//sort by closest distance
-		
-		//count classes of nearest neighbors
-		size_t nClasses = target->numLabels;
-		int * countClosestClasses = new int[nClasses];
-		for(size_t i = 0; i< nClasses; i++)
-			 countClosestClasses[i] = 0;
+        //sort by closest distance
 
-		for (int i = 0; i < k; i++)
-		{
+        //count classes of nearest neighbors
+        size_t nClasses = target->numLabels;
+        int * countClosestClasses = new int[nClasses];
+        for(size_t i = 0; i< nClasses; i++)
+            countClosestClasses[i] = 0;
+
+        for (int i = 0; i < k; i++)
+        {
 
 
-			int currentClass = data->label(idx[targetExample * dRows + i]);
-			countClosestClasses[currentClass]++;
-		}
+            int currentClass = data->label(idx[targetExample * dRows + i]);
+            countClosestClasses[currentClass]++;
+        }
 
-		//result: probability of class K for the example X
-		for(size_t i = 0; i < nClasses; i++)
-		{
-			results->pos(targetExample, i) = ((double)countClosestClasses[i]) / k;
-		}
-	}
+        //result: probability of class K for the example X
+        for(size_t i = 0; i < nClasses; i++)
+        {
+            results->pos(targetExample, i) = ((double)countClosestClasses[i]) / k;
+        }
+    }
 
-	//copy expected labels:
-	for (size_t i = 0; i < target->rows; i++)
-		results->label(i) = target->label(i);
+    //copy expected labels:
+    for (size_t i = 0; i < target->rows; i++)
+        results->label(i) = target->label(i);
     std::cout << "Average intrinsic time: " << static_cast<double>(totalCompute) / computeTimes  / 1000 << "s.\n";
-	return KNNResults(results);
+    return KNNResults(results);
 }
