f41b2034ff19a2025232419f22c1ff18a197286d
Yuan-Chuan-YUE <qyyuan@pllab.cs.nthu.edu.tw>
2021-01-09 23:49:53 +0800

    change to int

--- src/knn.cpp
+++ src/knn.cpp
@@ -20,12 +20,13 @@ KNNResults KNN::run(int k, DatasetPointer target) {
 	results->clear();
 
 	//squaredDistances: first is the distance; second is the trainExample row
-    unsigned long long tRows = target->rows;
-    unsigned long long dRows = data->rows;
+    int tRows = target->rows;
+    int dRows = data->rows;
+    int cols = data->cols;
     double * dist = (double *)malloc(sizeof(double) * tRows * dRows);
     double * ddist = (double *)malloc(sizeof(double) * tRows * k);
-    int * idx = (int *)malloc(sizeof(int) * tRows * dRows);
-    for(unsigned long long t = 0;t < tRows;++t){
+    long long * idx = (long long *)malloc(sizeof(long long) * tRows * dRows);
+    for(long long t = 0;t < tRows;++t){
         std::iota(idx + t * dRows, idx + (t + 1) * dRows, 0);
     }
 	//std::pair<double, int> * squaredDistances = new std::pair<double, int>[tRows * dRows];
@@ -33,9 +34,9 @@ KNNResults KNN::run(int k, DatasetPointer target) {
     //for(unsigned long long testTileBegin = 0; testTileBegin < dRows; testTileBegin += testTileSize){
 //#pragma omp parallel for schedule(static) num_threads(8)
         //for(unsigned long long trainTileBegin = 0; trainTileBegin < dRows; trainTileBegin += trainTileSize){
-    for (size_t trainExample = 0; trainExample < dRows; trainExample++) {
+    for (int trainExample = 0; trainExample < dRows; trainExample++) {
         #pragma omp parallel for schedule(static, 256) num_threads(8)
-        for(size_t targetExample = 0; targetExample < tRows; targetExample++) {
+        for(int targetExample = 0; targetExample < tRows; targetExample++) {
             /*
 #ifdef DEBUG_KNN
 if (targetExample % 100 == 0)
@@ -47,14 +48,14 @@ DEBUGKNN("Target %lu of %lu\n", targetExample, tRows);
             //squaredDistances[targetExample * dRows + trainExample].second = trainExample;
             int d = 0;
             double sum = 0;
-            for(; d < data->cols - 4; d += 4){
+            for(; d < cols - 4; d += 4){
                 double t0 = data->pos(trainExample, d) - target->pos(targetExample, d);
                 double t1 = data->pos(trainExample, d + 1) - target->pos(targetExample, d + 1);
                 double t2 = data->pos(trainExample, d + 2) - target->pos(targetExample, d + 2);
                 double t3 = data->pos(trainExample, d + 3) - target->pos(targetExample, d + 3);
                 sum += t0 * t0 + t1 * t1 + t2 * t2 + t3 * t3;
             }
-            for(; d < data->cols; ++d){
+            for(; d < cols; ++d){
                 double t0 = data->pos(trainExample, d) - target->pos(targetExample, d);
                 sum += t0 * t0;
             }
@@ -69,7 +70,7 @@ DEBUGKNN("Target %lu of %lu\n", targetExample, tRows);
 
     std::chrono::steady_clock::time_point b1 = std::chrono::steady_clock::now();
 #pragma omp parallel for schedule(static, 256) num_threads(8)
-    for(size_t targetExample = 0; targetExample < tRows; targetExample++) {
+    for(int targetExample = 0; targetExample < tRows; targetExample++) {
         std::partial_sort(idx + targetExample * dRows, 
                 idx + targetExample * dRows + k,
                 idx + (targetExample + 1) * dRows,
@@ -78,7 +79,7 @@ DEBUGKNN("Target %lu of %lu\n", targetExample, tRows);
                 });
     }
 #pragma omp parallel for num_threads(8)
-    for(unsigned long long t = 0; t < tRows * k; t++) {
+    for(int t = 0; t < tRows * k; t++) {
         int targetExample = t / tRows;
         int i = t % tRows;
         ddist[targetExample * k + i] = dist[idx[i]];
@@ -91,7 +92,7 @@ DEBUGKNN("Target %lu of %lu\n", targetExample, tRows);
 
 
 #pragma omp parallel for schedule(static) num_threads(8)
-    for(size_t targetExample = 0; targetExample < tRows; targetExample++) {
+    for(int targetExample = 0; targetExample < tRows; targetExample++) {
 
 
         //sort by closest distance
@@ -118,7 +119,7 @@ DEBUGKNN("Target %lu of %lu\n", targetExample, tRows);
     }
 
     //copy expected labels:
-    for (size_t i = 0; i < target->rows; i++)
+    for (int i = 0; i < tRows; i++)
         results->label(i) = target->label(i);
     std::cout << "Average intrinsic time: " << static_cast<double>(totalCompute) / computeTimes  / 1000 << "s.\n";
     return KNNResults(results);
