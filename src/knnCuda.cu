fe56faf8d1c0f8b028baa54b74c1ec5525bc4470
cindyKaulitz <tony320cindy@gmail.com>
2021-01-11 03:10:21 +0800

    block ver, good accuracy low speed

--- src/knnCuda.cu
+++ src/knnCuda.cu
@@ -16,63 +16,32 @@
 
 using namespace std;
 
-// double GetSquaredDistance(DatasetPointer train, size_t trainExample, DatasetPointer target, size_t targetExample) {
-// 	assert(train->cols == target->cols);
-// 	double sum = 0;
-// 	double difference;
-// 	// printf("train->cols = %d\n",train->cols);
-// 	for(size_t col = 0; col < train->cols; col++) {
-// 		difference = train->pos(trainExample, col) - target->pos(targetExample, col);
-// 		sum += difference * difference;
-// 	}
-// 	return sum;
-// }
-__device__ double CudaGetSqDist(int cols, int trainExample, int targetExample, double *train, double *target) {
-	double sum = 0;
-    double difference;
-    
-	for(int col = 0; col < cols; col++) {
-        difference = train[col] - target[col];
-        sum += difference * difference;
-        
-    }
-	return sum;
-}
 
-__global__ void CalDistance(int &dataCol, int &dataRow, int &e_t, int &e_b, int &r_t, int &r_b,
+__global__ void CalDistance(int dataCol, int dataRow, int targetRow, int start_round,
 							double *train, double *target, 
 							double *sq_dist, int *sq_example){
-	
+
+    int x = threadIdx.x;
+    int y = threadIdx.y;
+    int targetExample = blockIdx.x  * 32 + x;
+    int trainExample = (blockIdx.y + start_round) * 32 + y;
+    if(trainExample >= dataRow || targetExample >= targetRow){
+        return;
+    }
     
-	// int targetExample = blockIdx.x;
-    // int trainExample = threadIdx.x;
-    int cols = dataCol;
-    int start_b = blockIdx.x * e_b;
-    int num_b = e_b;
-    if(r_b > 0 && blockIdx.x < r_b-1){
-        start_b = blockIdx.x * (e_b+1);
-        num_b = e_b+1;
+    // double dist = 0; // = CudaGetSqDist(dataCol, trainExample, targetExample, &train[trainExample*dataCol], &target[targetExample*dataCol]);
+    double sum = 0;
+	double difference;
+    // printf("train->cols = %d\n",cols);
+    // #pragma unroll 
+	for(int col = 0; col < dataCol; col++) {
+        difference = train[trainExample*dataCol+col] - target[targetExample*dataCol+col];
+        sum += difference * difference;
+        
+        // printf("col = %d sum = %f\n",col, sum);
     }
-    int start_t = threadIdx.x * e_t;
-    int num_t = e_t;
-    if(r_t > 0 && threadIdx.x < r_t-1){
-        start_t = threadIdx.x * (e_t+1);
-        num_t = e_t+1;
-    }
-    // printf("start_b = %d, start_t= %d\n", start_b, start_t);
-    // printf("blockIdx.x = %d, threadIdx.x = %d\n", blockIdx.x, threadIdx.x);
-
-    for(int targetExample = start_b; targetExample < start_b+num_b; targetExample++ ){
-        for(int trainExample = start_t; trainExample < start_t+num_t; trainExample++){
-            double dist = CudaGetSqDist(dataCol, trainExample, targetExample, &train[trainExample*cols], &target[targetExample*cols]);
-            sq_dist[targetExample * dataRow + trainExample] = dist;
-            sq_example[targetExample * dataRow + trainExample] = trainExample;
-        }
-    }
-
-    // double dist = CudaGetSqDist(dataCol, trainExample, targetExample, &train[trainExample*cols], &target[targetExample*cols]);
-    // sq_dist[targetExample * dataRow + trainExample] = dist;
-    // sq_example[targetExample * dataRow + trainExample] = trainExample;
+    sq_dist[targetExample * dataRow + trainExample] = sum;
+    sq_example[targetExample * dataRow + trainExample] = trainExample;
     // __syncthreads();
 
 
@@ -84,6 +53,7 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
     
 	DatasetPointer results(new dataset_base(target->rows,target->numLabels, target->numLabels));
 	results->clear();
+    // printf("first\n");
     
 	//squaredDistances: first is the distance; second is the trainExample row
 	std::pair<double, int> squaredDistances[data->rows];
@@ -91,6 +61,8 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
 	int *squaredDistances_second = (int*)malloc(data->rows* target->rows* sizeof(int));
     
 	
+    
+	// DatasetPointer *device_target;
     
     int deviceID;
     cudaDeviceProp prop;
@@ -100,6 +72,7 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
     if (!prop.deviceOverlap){
         printf("!prop.deviceOverlap\n");
     }
+    // printf("second\n");
 
     int data_rows = (int)data->rows;
     int data_cols = (int)data->cols;
@@ -110,38 +83,45 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
 
     // thread limit 1024
     // block limit 65535
-    int block_size = 2000;
-    int thread_size = 1024;
+    // int block_size = 50000;
+    // int thread_size = 1024;
+
+    int B = 32;
+    // int B_x = 65535/target_rows - 1; 
+    int round_train = (data_rows%B==0)?(data_rows/B):(data_rows/B + 1); 
+    int start_round = 0;
+    dim3 grid(round_train, target_rows);
+    // dim3 grid(target_rows, round_train);
+    dim3 blk(B, B);
+    // printf("round_train = %d\n", round_train);
 
     
-    int *e_t;
-    int *e_b;
-    int *r_t;
-    int *r_b;
+    // int *e_t;
+    // int *e_b;
+    // int *r_t;
+    // int *r_b;
     
-    if(data_rows < thread_size){
-        thread_size = data_rows;
-    }
-    if(target_rows < block_size ){
-        block_size = target_rows;
-    }
+    // if(data_rows < thread_size){
+    //     thread_size = data_rows;
+    // }
+    // if(target_rows < block_size ){
+    //     block_size = target_rows;
+    // }
 
-    int ele_per_threads = data_rows/thread_size;
-    int ele_per_blocks = target_rows/block_size;
-    int res_thread = 0;
-    int res_block = 0;
+    // int ele_per_threads = data_rows/thread_size;
+    // int ele_per_blocks = target_rows/block_size;
+    // int res_thread = 0;
+    // int res_block = 0;
 
-    if (data_rows%thread_size > 0){
-        res_thread = data_rows%thread_size;
-    }
+    // if (data_rows%thread_size > 0){
+    //     res_thread = data_rows%thread_size;
+    // }
     
-    if (target_rows%block_size > 0){
-        res_block = target_rows%block_size;
-    }
+    // if (target_rows%block_size > 0){
+    //     res_block = target_rows%block_size;
+    // }
     
-    // printf("block_size %d, thread_size %d\n", block_size, thread_size);
-    // printf("e_t %d, e_b %d, r_t %d, r_b %d\n", ele_per_threads,ele_per_blocks, res_thread, res_block);
-    
+
     
 	int *d_data_rows ;
 	int *d_data_cols ;
@@ -155,26 +135,28 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
     
     
     cudaSetDevice(0);
+    // cudaStream_t stream;
+    // cudaStreamCreate(&stream);
 
-    cudaMalloc((void **)&e_b,  sizeof(int));
-    cudaMalloc((void **)&e_t,  sizeof(int));
-    cudaMalloc((void **)&r_b,  sizeof(int));
-    cudaMalloc((void **)&r_t,  sizeof(int));
+    // cudaMalloc((void **)&e_b,  sizeof(int));
+    // cudaMalloc((void **)&e_t,  sizeof(int));
+    // cudaMalloc((void **)&r_b,  sizeof(int));
+    // cudaMalloc((void **)&r_t,  sizeof(int));
 
-    cudaMalloc((void **)&d_data_cols,  sizeof(int));
-    cudaMalloc((void **)&d_data_rows,  sizeof(int));
+    // cudaMalloc((void **)&d_data_cols,  sizeof(int));
+    // cudaMalloc((void **)&d_data_rows,  sizeof(int));
     cudaMalloc(&d_data_pos,  data_cols * data_rows * sizeof(double));
     cudaMalloc(&d_target_pos,  data_cols * target_rows * sizeof(double));
     cudaMalloc(&d_sd_dist,  data_rows * target_rows *  sizeof(double));
     cudaMalloc(&d_sd_example,  data_rows * target_rows * sizeof(int));
     
-    cudaMemcpy(e_b, &ele_per_blocks, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(e_t, &ele_per_threads, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(r_b, &res_block, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(r_t, &res_thread, sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(e_b, &ele_per_blocks, sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(e_t, &ele_per_threads, sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(r_b, &res_block, sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(r_t, &res_thread, sizeof(int), cudaMemcpyHostToDevice);
 
-    cudaMemcpy(d_data_cols, &data_cols, sizeof(int), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_data_rows, &data_rows, sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_data_cols, &data_cols, sizeof(int), cudaMemcpyHostToDevice);
+    // cudaMemcpy(d_data_rows, &data_rows, sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(d_data_pos, data_pos,  data_cols * data_rows * sizeof(double), cudaMemcpyHostToDevice);
     cudaMemcpy(d_target_pos, target_pos,  data_cols * target_rows * sizeof(double), cudaMemcpyHostToDevice);
     
@@ -183,20 +165,28 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
     
 
 
-	CalDistance<<<block_size, thread_size>>>(*d_data_cols, *d_data_rows, *e_t, *e_b, *r_t, *r_b, d_data_pos, d_target_pos, d_sd_dist, d_sd_example);
-
+    // printf("third\n");
+    #pragma omp parallel nowait
+    for(int i = 0; i < round_train; i++){
+        CalDistance<<<grid, blk>>>(data_cols, data_rows, target_rows, (i*round_train), d_data_pos, d_target_pos, d_sd_dist, d_sd_example);
+        // start_round += round_train;
+    }
+    // cudaStreamSynchronize(stream);
+    // cudaStreamDestroy(stream);
+    // printf("forth\n");
     
-	cudaMemcpy(squaredDistances_first , d_sd_dist, data_rows * target_rows * sizeof(double), cudaMemcpyDeviceToHost);
-	cudaMemcpy(squaredDistances_second , d_sd_example, data_rows * target_rows * sizeof(int), cudaMemcpyDeviceToHost);
-    
-    cudaFree(d_data_cols);
-    cudaFree(d_data_rows);
+	cudaMemcpyAsync(squaredDistances_first , d_sd_dist, data_rows * target_rows * sizeof(double), cudaMemcpyDeviceToHost);
+    // printf("fifth-1\n");
+	cudaMemcpyAsync(squaredDistances_second , d_sd_example, data_rows * target_rows * sizeof(int), cudaMemcpyDeviceToHost);
+    // printf("fifth-2\n");
+    // cudaFree(d_data_cols);
+    // cudaFree(d_data_rows);
     cudaFree(d_data_pos);
     cudaFree(d_target_pos);
     cudaFree(d_sd_dist);
     cudaFree(d_sd_example);
 
-    // #pragma omp parallel for shared(data, squaredDistances)
+    #pragma omp parallel for schedule(guided) shared(data, squaredDistances)
 	for(size_t targetExample = 0; targetExample < target->rows; targetExample++) {
 
         
@@ -207,6 +197,13 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
             
         }
 
+        // for (int j = 0; j < 5; j++){
+        //     if(targetExample < 5){
+        //         // printf("squaredDistances_first[targetExample *data->rows + j] = %f\n", squaredDistances_first[targetExample *data->rows + j]);
+        //         printf("squaredDistances_second[targetExample *data->rows + j] = %d\n", squaredDistances_second[targetExample *data->rows + j]);
+        //         // printf("squaredDistances target(%d) ,train(%d) = %f\n",targetExample,j, squaredDistances[j].first, squaredDistances[j].second);
+        //     }
+        // }
 		//sort by closest distance
         sort(squaredDistances, squaredDistances + data->rows);
 
@@ -227,6 +224,7 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
 		for(size_t i = 0; i < nClasses; i++)
 		{
             results->pos(targetExample, i) = ((double)countClosestClasses[i]) / k;
+            // printf("target %d = %d \n", targetExample, results->pos(targetExample, i));
 		}
 	}
 
@@ -242,3 +240,4 @@ KNNResults KNNCUDA::run(int k, DatasetPointer target) {
 
 	return KNNResults(results);
 }
+
